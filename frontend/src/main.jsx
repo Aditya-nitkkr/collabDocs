138509b96d05df2ebb89aa3c8cf5386b2c61679c
Aditya-nitkkr <raoa7405@gmail.com>
2026-05-14 22:10:15 +0530

    correct the app.js

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,12 +4,18 @@ import { AuthProvider } from './context/AuthContext'
 import { SocketProvider } from './context/SocketContext';
 import App from './App.jsx';
 import { Toaster } from 'react-hot-toast';
+import { BrowserRouter } from 'react-router-dom';
 
 createRoot(document.getElementById('root')).render(
-  <AuthProvider>
-    <Toaster position="top-right" reverseOrder={false} />
-    <SocketProvider>
-      <App />
-    </SocketProvider>
-  </AuthProvider>,
+  <BrowserRouter>
+    <AuthProvider>
+      <Toaster position="top-right" reverseOrder={false} />
+
+      <SocketProvider>
+        <App />
+      </SocketProvider>
+
+      
+    </AuthProvider>
+  </BrowserRouter>
 )
